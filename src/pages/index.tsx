32c0f32e1b9f2e5bd93e51665cfe108eb168c5d7
池野朱里 <ikenoshuri@ikenojurinoMacBook-Pro.local>
2023-10-21 16:31:51 +0900

    ChakraUIセット完了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {Box,Heading} from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 import type { NextPage } from 'next'
 
 const Page: NextPage = () => {
-  return <div>home</div>
+  return (
+    <Box>
+      <Heading>Chakra UI</Heading>
+    </Box>
+  )
 }
 
 export default Page
